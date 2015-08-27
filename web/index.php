8fce7d48fcce82be8b8210c029a2d52032c03b7d
eloir <eloir@new-host-2.home>
2015-08-27 20:43:31 -0300

    Novas Rotas

--- web/index.php
+++ web/index.php
@@ -11,6 +11,8 @@ $app = new \Slim\Slim(
 $app->get('/hello/:name', 'Controller\Foo:bar');
 $app->get('/', 'Controller\IndexController:index');
 $app->get('/cadastro', 'Controller\IndexController:cadastros');
+$app->get('/api/v1/areas', 'Controller\AreaController:listAreasAction');
+$app->get('/api/v1/areas/:id', 'Controller\AreaController:getAreaAction')->name('area_by_id');
 /* $app->get('/api/v1/brands', 'Controller\BrandController:listBrandsAction');
  * $app->get('/api/v1/brands/:id', 'Controller\BrandController:getBrandAction')->name('brand_by_id');
  * $app->put('/api/v1/brands/:id', 'Controller\BrandController:putBrandAction')->name('brand_put_by_id');
